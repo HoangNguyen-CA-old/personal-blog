1c9ef15d7c5f4865d66a6794942013df03f00a96
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-07-21 10:18:22 -0400

    re-added footer component with main wrapper to ensure it stays at bottom of the screen

--- client/src/containers/Layout/Layout.js
+++ client/src/containers/Layout/Layout.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styles from './Layout.module.scss';
 
 import Navbar from '../Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 
 export class Layout extends Component {
   render() {
     return (
       <>
         <Navbar></Navbar>
-        {this.props.children}
+        <div className={styles.Container}>{this.props.children}</div>
+        <Footer></Footer>
       </>
     );
   }
